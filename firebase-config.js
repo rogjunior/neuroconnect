41687837ea0d7f1fbaaa2e3a5287a072e5067db5
rogjunior <98099357+rogjunior@users.noreply.github.com>
2025-10-20 21:46:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
+// Este arquivo será usado no futuro para conectar com Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJETO_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicialização futura:
+// firebase.initializeApp(firebaseConfig);
 
