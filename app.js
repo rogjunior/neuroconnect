170d0285d8865deef14b5d726757f5729f267540
rogjunior <98099357+rogjunior@users.noreply.github.com>
2025-10-20 21:23:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Simulação de login e redirecionamento
+  window.location.href = "home.html";
+});
